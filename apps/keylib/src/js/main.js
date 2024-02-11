97d23d521b8c1af096007e89faee926f2bff163d
Gavin Francis <gav@eh7.co.uk>
2024-02-11 18:08:22 +0000

    adding example wallet interface using parcel

--- apps/keylib/src/js/main.js
+++ apps/keylib/src/js/main.js
@@ -7,3 +7,17 @@ document.querySelectorAll('[data-bs-toggle="popover"]')
     new Popover(popover)
   })
 
+// Create an example popover2
+document.querySelectorAll('[data-bs-toggle="popover2"]')
+  .forEach(popover2 => {
+    new Popover(popover2)
+  })
+
+// check if key is setup
+if (localStorage.getItem("keyset")) {
+  let keyset = localStorage.getItem("name");
+  console.log('localStorage.getItem.keyset:', keyset);
+} else {
+  console.log('localStorage.getItem.keyset: false');
+}
+
