540875290ac50c72d7ad1a6a6bde718f1d19292d
Gavin Francis <gav@eh7.co.uk>
2024-02-13 12:11:55 +0000

    update example wallet interface using parcel, add overlay for no key data, and overlay styles

---
+++ apps/keylib/src/js/functions.js
@@ -0,0 +1,9 @@
+window.divOff = function divOff(_id) {
+  document.getElementById(_id).style.display = "none";
+}
+
+
+//function test() {
+module.exports.test = function test() {
+  alert('test')
+}
