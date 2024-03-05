e483262c057d3c880de4710a3f0d1986df852254
Gavin Francis <gav@eh7.co.uk>
2024-03-05 23:10:43 +0000

    keylib react user interface - add scaffold example for sub menu dropdowns, WIP intergrate into network link in nav to have sub menue options

--- apps/keylib/src/js/components/Nav/Main.js
+++ apps/keylib/src/js/components/Nav/Main.js
@@ -1,5 +1,13 @@
-import Nav from 'react-bootstrap/Nav';
 import Container from "react-bootstrap/Container";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import {
+  DropdownSubmenu,
+  NavDropdownMenu,
+} from "react-bootstrap-submenu";
 
 function NavMain() {
   return (
@@ -22,8 +30,32 @@ function NavMain() {
         </Nav.Item>
         <Nav.Item className="ml-auto">
           <Nav.Link href="/config/network">Network</Nav.Link>
+          <NavDropdown.Item href="/config/network">Network</NavDropdown.Item>
+          <DropdownSubmenu href="#action/3.7" title="Text to show">
+            <NavDropdown.Item href="#action/8.1">Sub 1</NavDropdown.Item>
+          </DropdownSubmenu>
         </Nav.Item>
       </Nav>
+      <Navbar>
+        <Navbar.Brand href="#home">Crypto Wallet Interface</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">test</Nav.Link>
+            <NavDropdownMenu title="Dropdown 1" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <DropdownSubmenu href="#action/3.7" title="Text to show">
+                <NavDropdown.Item href="#action/8.1">Sub 1</NavDropdown.Item>
+                <DropdownSubmenu href="#action/3.7" title="Text to show">
+                  <NavDropdown.Item href="#action/9.1">
+                    Sub 2
+                  </NavDropdown.Item>
+                </DropdownSubmenu>
+              </DropdownSubmenu>
+            </NavDropdownMenu>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </Container>
   );
 }
