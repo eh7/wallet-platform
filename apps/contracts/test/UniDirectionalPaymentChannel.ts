40e9b02795aca0061d2db9af17cb54a2627e801e
Gavin Francis <gav@eh7.co.uk>
2024-04-19 17:17:01 +0100

    solidity contract example - update scaffold UniDirectionalPaymentChannel contract and tests

--- apps/contracts/test/UniDirectionalPaymentChannel.ts
+++ apps/contracts/test/UniDirectionalPaymentChannel.ts
@@ -6,10 +6,15 @@ describe("UniDirectionalPaymentChannel contract", function () {
 
   it("Deployment the UniDirectionalPaymentChannel contract", async function () {
     const [owner] = await ethers.getSigners();
+    const receiver = (await ethers.getSigners())[1];
 
-    const uniDirectionalPaymentChannelContract = await ethers.deployContract("UniDirectionalPaymentChannel", [owner.address]);
+    //const uniDirectionalPaymentChannelContract = await ethers.deployContract("UniDirectionalPaymentChannel", [owner.address]);
+    const uniDirectionalPaymentChannelContract = await ethers.deployContract("UniDirectionalPaymentChannel", [receiver.address]);
 
-//     expect(await uniDirectionalPaymentChannelContract.owner()).to.equal(owner.address);
+    //console.log((await ethers.getSigners())[1].address);
+
+    expect(await uniDirectionalPaymentChannelContract.sender()).to.equal(owner.address);
+    expect(await uniDirectionalPaymentChannelContract.receiver()).to.equal(receiver.address);
 
     //const ownerBalance = await hardhatToken.balanceOf(owner.address);
     //expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
